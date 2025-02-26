b8f65e2d2922bc49a41f3da70780075c38ef566a
Glassgobben <markuscehic21@gmail.com>
2025-02-26 16:52:45 +0100

    navbar fixed

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ const navbar = document.querySelector('header');
 
 window.addEventListener('scroll', function () {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const navHighlight = document.getElementsByClassName("nav_highlight");
 
     if (scrollTop < lastScrollTop) {
         // När användaren scrollar uppåt, visa navbar
-        navbar.style.top = "0";
-        navbar.style.position = "fixed";
+        if (navHighlight.length === 0) {
+            navbar.classList.add("nav_highlight");
+        }
     } else {
         // När användaren scrollar neråt, göm navbar
-        navbar.style.top = "-100px";
-        navbar.style.position = "absolut";
+        navbar.classList.remove("nav_highlight");
     }
 
     lastScrollTop = scrollTop;
